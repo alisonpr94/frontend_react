3c74a18e59d18d8f8b577a7188b6cdd96b3ac65b
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-11-19 20:33:57 -0300

    Projeto cafeteria frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [produtos, setProdutos] = useState([]);
+  const [vendas, setVendas] = useState([]);
+
+  useEffect(() => {
+    // Carregue os produtos do backend
+    axios.get('http://localhost:8000/api/produtos/')
+      .then(response => setProdutos(response.data))
+      .catch(error => console.error('Erro ao buscar produtos:', error));
+
+    // Carregue as vendas do backend
+    axios.get('http://localhost:8000/api/vendas/')
+      .then(response => setVendas(response.data))
+      .catch(error => console.error('Erro ao buscar vendas:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cafeteria App</h1>
+
+      <h2>Produtos</h2>
+      <ul>
+        {produtos.map(produto => (
+          <li key={produto.id}>{produto.nome} - R${produto.preco}</li>
+        ))}
+      </ul>
+
+      <h2>Vendas</h2>
+      <ul>
+        {vendas.map(venda => (
+          <li key={venda.id}>{venda.total} - {venda.data}</li>
+        ))}
+      </ul>
     </div>
   );
 }
